c03841cde27c71ce95a10cfc1c283b30ad12f964
Edmar Felipe <edmarfelipe@outlook.com>
2020-02-11 22:54:50 -0300

    feat: add archive page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react'
+import GlobalStyle from './styles/global'
+
+import Archive from './pages/Archive'
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Archive />
+    </>
   )
 }

070072b8ec2c2f543bcf520dc9a0631f3043ce79
Kalleu Oliveira <xkalleu@gmail.com>
2020-02-12 23:26:26 -0300

    feat: basic card style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import GlobalStyle from './styles/global'
-
+import Card from './components/Card'
 import Archive from './pages/Archive'
 
 export default function App() {
@@ -8,6 +8,7 @@ export default function App() {
     <>
       <GlobalStyle />
       <Archive />
+      <Card />
     </>
   )
 }

ec78941cbdb798051712f4b06cf51e86295bb447
Edmar Felipe <edmarfelipe@outlook.com>
2020-02-15 17:13:57 -0300

    refactor: add props to card component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import GlobalStyle from './styles/global'
-import Card from './components/Card'
 import Archive from './pages/Archive'
 import SideMenu from './components/SideMenu'
+import Header from './components/Header'
 
 export default function App() {
+  const menuItems = [
+    'Shipping',
+    'Blog',
+    'Company',
+    'My account',
+  ]
+
   return (
     <>
       <GlobalStyle />
-      <Archive />
+      <Header title="Amazing store" cityName="Los Angeles" tel="8 800 505-04-83" menuItems={menuItems} />
       <SideMenu />
-      <Card />
+      <Archive />
     </>
   )
 }

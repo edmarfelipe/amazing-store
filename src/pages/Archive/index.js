cd776982e4ef3e0d8193e855e85b2749848ecb99
Edmar Felipe <edmarfelipe@outlook.com>
2020-02-15 17:21:27 -0300

    feat: add alt to the imgs

--- src/pages/Archive/index.js
+++ src/pages/Archive/index.js
@@ -10,19 +10,19 @@ export default function Archive() {
       <Card
         title="Missha All Around Safe Block Sebum Zero Sun"
         imageUrl="./assets/imgs/product-1.png"
-        imageAlt=""
+        imageAlt="Block Sebum Zero Sun"
         price="4.00"
       />
       <Card
         title="Innisfree Green Tea Balancing Cream"
         imageUrl="./assets/imgs/product-2.png"
-        imageAlt=""
+        imageAlt="Green Tea"
         price="18.00"
       />
       <Card
         title="Claire’s Cloud 9 Blanc De Whitening Cream"
         imageUrl="./assets/imgs/product-3.png"
-        imageAlt=""
+        imageAlt="Whitening Cream"
         price="4.00"
       />
     </Container>

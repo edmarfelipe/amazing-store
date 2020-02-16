aaade95e6f6789fe76481d7383c211479b47ac38
Daniel Gomes Pereira <danielgomesp@gmail.com>
2020-02-16 14:13:15 -0300

    refactor: added props to SideMenu components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import Card from './components/Card'
 import Archive from './pages/Archive'
 import SideMenu from './components/SideMenu'
 
+const categories = [
+  {
+    title: 'Sala',
+    items: ['sofá', 'tv'],
+  },
+  {
+    title: 'Quarto',
+    items: ['cama', 'Guarda-roupas'],
+  },
+  {
+    title: 'Cozinha',
+    items: ['Geladeira', 'Fogão', 'Liquidificador'],
+  },
+]
+
 export default function App() {
   return (
     <>
       <GlobalStyle />
       <Archive />
-      <SideMenu />
+      <SideMenu categories={categories} />
       <Card />
     </>
   )

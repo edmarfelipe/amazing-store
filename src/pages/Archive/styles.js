f50d258248e7ccdd81ba30881ab104996dea13a7
Edmar Felipe <edmarfelipe@outlook.com>
2020-02-18 23:13:06 -0300

    test: remove root component test
    feat: add size card

--- src/pages/Archive/styles.js
+++ src/pages/Archive/styles.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import Variables from '../../styles/variables'
 
 export const Container = styled.ul`
   display: grid;
-  grid-gap: 10px;
+  grid-gap: 28px;
   grid-template-columns: repeat(4, 1fr);
+  z-index: ${Variables.zIndex.lvTwo};
 `
 
 export const Grid = styled.div`

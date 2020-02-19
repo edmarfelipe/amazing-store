fa5fb074052f8615505e41e73fc641fb7cd42f23
Edmar Felipe <edmarfelipe@outlook.com>
2020-02-18 23:51:12 -0300

    feat: added styling to Title, Category and Item

--- src/components/SideMenu/styles.js
+++ src/components/SideMenu/styles.js
@@ -13,19 +13,19 @@ export const Container = styled.div`
 
 export const Title = styled.h2`
   color: ${variables.tertiaryColor};
-  font-size: ${variables.fontSizes.base};
+  font-size: ${variables.fontSize};
   height: 20px;
   line-height: ${variables.lineHeight};
 `
 
 export const Category = styled.h2`
-  font-size: ${variables.fontSizes.base};
+  font-size: ${variables.fontSize};
   margin-top: 10px;
 `
 
 export const Item = styled.p`
   color: ${(props) => props.isActive ? variables.primaryColor : variables.quaternaryColor};
-  font-size: ${variables.fontSizes.base};
+  font-size: ${variables.fontSize};
   line-height: ${variables.lineHeight};
   margin-top: 10px;
 `

1e22ae814e404b234f84003df4f6d3d403d2981d
Daniel Gomes Pereira <danielgomesp@gmail.com>
2020-02-20 00:24:19 -0300

    feat: add funtionality to toogle items

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,19 +1,33 @@
-/* eslint-disable react/no-array-index-key */
-import React from 'react'
+/* eslint-disable no-unused-expressions */
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import {
   Container, Title, Category, Items, Item,
 } from './styles'
 
 export default function SideMenu({ categories }) {
-  const listItems = categories.map((category, categoryIndex) => (
-    <Category key={categoryIndex}>
-      <Title>{category.title}</Title>
-      <Items>
-        {
-          category.items.map((item, itemIndex) => <Item key={itemIndex}>{item}</Item>)
-        }
-      </Items>
+  const [activeCategory, setActiveCategory] = useState(1)
+
+  function showCategory(categoryId) {
+    if (categoryId === activeCategory) {
+      setActiveCategory(0)
+      return
+    }
+    setActiveCategory(categoryId)
+  }
+
+  const listItems = categories.map((category) => (
+    <Category key={category.id.toString()}>
+      <Title onClick={() => showCategory(category.id)}>{category.title}</Title>
+      {
+        activeCategory === category.id
+          ? (
+            <Items>
+              {category.items.map((item) => <Item key={item.id}>{item.title}</Item>)}
+            </Items>
+          ) : null
+      }
+
     </Category>
   ))
   return (

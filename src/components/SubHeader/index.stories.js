d8a8e2922839c432cade0dc696f6b54cab94cc04
Kalleu Oliveira <xkalleu@gmail.com>
2020-03-09 22:52:17 -0300

    feat: create component storybook

---
+++ src/components/SubHeader/index.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SubHeader from './index'
+
+export default {
+  title: 'SubHeader',
+  component: SubHeader,
+}
+
+export function Default() {
+  return (
+    <SubHeader />
+  )
+}

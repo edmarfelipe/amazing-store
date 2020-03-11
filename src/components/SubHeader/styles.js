aa5404eba71edb7652cca240c973474dca6779ee
Kalleu Oliveira <xkalleu@gmail.com>
2020-03-11 09:27:44 -0300

    feat: create area filters subheader (#22)
    * feat: create component storybook
    * feat: create area filters subheader

--- src/components/SubHeader/styles.js
+++ src/components/SubHeader/styles.js
@@ -3,15 +3,33 @@ import styled from 'styled-components'
 import Variables from 'styles/variables'
 
 export const Container = styled.div`
-  align-items: center;
-  background-color: ${Variables.backgroundColor};
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  border-bottom: 2px solid #E1E1E1;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 1fr 50px;
+  margin: 50px 0;
 `
+
+export const Filters = styled.div`
+  color: ${Variables.quaternaryColor};
+  font-size: ${Variables.fontSizes.base};
+
+  &:last-child {
+    justify-self: end;
+  }
+`
+export const FiltersName = styled.p`
+  color: ${Variables.secondaryColor};
+  display: inline-block;
+  font-size: ${Variables.fontSizes.base};
+  margin-left: 5px;
+  text-transform: uppercase;
+`
+
 export const Paginator = styled.div`
   align-items: center;
   display: flex;
+  text-transform: uppercase;
 `
 
 export const PaginatorTitle = styled.p`
@@ -20,7 +38,21 @@ export const PaginatorTitle = styled.p`
   margin-right: 10px;
 `
 
+export const Row = styled.div`
+  align-items: center;
+  background-color: ${Variables.backgroundColor};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const RowFilters = styled.div`
+  align-items: center;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`
+
 export const Title = styled.p`
-  font-family: ${Variables.fontFamilyBold};
+  font-family: ${Variables.fontFamily};
   font-size: ${Variables.fontSizes.lg};
 `

018b96397a7f2615ffb1fa0d49846748f56f791c
Kalleu Oliveira <xkalleu@gmail.com>
2020-03-10 22:19:32 -0300

    feat: create area filters subheader

--- src/components/SubHeader/index.js
+++ src/components/SubHeader/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-  Container, Paginator, PaginatorTitle, Title,
+  Container, Filters, FiltersName, Paginator, PaginatorTitle, Row, RowFilters, Title,
 } from './styles'
 
 import Icon from '../Icon'
@@ -8,12 +8,32 @@ import Icon from '../Icon'
 export default function SubHeader() {
   return (
     <Container>
-      <Title>Creams & gels</Title>
-      <Paginator>
-        <PaginatorTitle>Showing 60 of 392</PaginatorTitle>
-        <Icon name="arrowleft" width={25} />
-        <Icon name="arrowright" width={25} />
-      </Paginator>
+      <Row>
+        <Title>Creams & gels</Title>
+        <Paginator>
+          <PaginatorTitle>Showing 60 of 392</PaginatorTitle>
+          <Icon name="arrowleft" width={25} />
+          <Icon name="arrowright" width={25} />
+        </Paginator>
+      </Row>
+      <RowFilters>
+        <Filters>
+          BRAND:
+          <FiltersName>ALL BRANDS</FiltersName>
+        </Filters>
+        <Filters>
+          SKIN TYPE:
+          <FiltersName>FOR ONLY SKIN</FiltersName>
+        </Filters>
+        <Filters>
+          AGE:
+          <FiltersName>30+</FiltersName>
+        </Filters>
+        <Filters>
+          SORT:
+          <FiltersName>BY POPULARITY</FiltersName>
+        </Filters>
+      </RowFilters>
     </Container>
   )
 }

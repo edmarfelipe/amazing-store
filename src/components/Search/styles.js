5cda0383967579679b8afbadfdf56c9b61282e9b
danielgomesp <danielgomesp@gmail.com>
2020-03-21 22:25:46 -0300

    feat: add search button

---
+++ src/components/Search/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+ align-items: center;
+ border-radius: 3px;
+ color: ${({ color }) => color};
+ display: flex;
+ height: 50px;
+ padding: 10px;
+ width: 660px;
+`
+export const Text = styled.p`
+  margin: 0 5px;
+`

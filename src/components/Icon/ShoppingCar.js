5b3f644bc6a14239cc7cda561cc5263e2fa523be
Kalleu Oliveira <xkalleu@gmail.com>
2020-03-24 22:44:46 -0300

    feat: add proptypes icon cart

--- src/components/Icon/ShoppingCar.js
+++ src/components/Icon/ShoppingCar.js
@@ -11,19 +11,17 @@ export default function ShoppingCar({ color, width }) {
       xmlnsXlink="http://www.w3.org/1999/xlink"
     >
       <defs>
-        <path d="M0.820513 1.6C1.27367 1.6 1.64103 1.24183 1.64103 0.8C1.64103 0.358172 1.27367 0 0.820513 0C0.367356 0 0 0.358172 0 0.8C0 1.24183 0.367356 1.6 0.820513 1.6Z" transform="translate(6.15387 12.3999)" id="path_1" />
-        <path d="M0.820513 1.6C1.27367 1.6 1.64103 1.24183 1.64103 0.8C1.64103 0.358172 1.27367 0 0.820513 0C0.367356 0 0 0.358172 0 0.8C0 1.24183 0.367356 1.6 0.820513 1.6Z" transform="translate(11.0769 12.3999)" id="path_2" />
+        <path d="M.820513 1.6c.453157 0 .820517-.35817.820517-.8 0-.441828-.36736-.8-.820517-.8C.367356 0 0 .358172 0 .8c0 .44183.367356.8.820513.8z" transform="translate(6.153846 12.4)" id="a" />
+        <path d="M.820513 1.6c.453157 0 .820517-.35817.820517-.8 0-.441828-.36736-.8-.820517-.8C.367356 0 0 .358172 0 .8c0 .44183.367356.8.820513.8z" transform="translate(11.07692 12.4)" id="b" />
       </defs>
-      <g id="Group-3" transform="translate(2 2)">
-        <path d="M0 0L2.05128 0L4.92308 8.8L13.9487 8.8L16 2.4L2.87179 2.4" id="Path-367" fill="none" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
-        <g id="Oval-43">
-          <use xlinkHref="#path_1" fill="none" stroke={color} strokeWidth="4" />
-          <use stroke="none" fill={color} fillOpacity="0" xlinkHref="#path_1" />
-        </g>
-        <g id="Oval-43">
-          <use xlinkHref="#path_2" fill="none" stroke={color} strokeWidth="4" />
-          <use stroke="none" fill={color} fillOpacity="0" xlinkHref="#path_2" />
-        </g>
+      <path d="M2 2h2.05128l2.8718 8.8h9.02562L18 4.4H4.87179" fill="none" stroke={color} strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+      <g transform="translate(2 2)">
+        <use xlinkHref="#a" fill="none" stroke={color} strokeWidth="4" />
+        <use fill={color} fillOpacity="0" xlinkHref="#a" />
+      </g>
+      <g transform="translate(2 2)">
+        <use xlinkHref="#b" fill="none" stroke={color} strokeWidth="4" />
+        <use fill={color} fillOpacity="0" xlinkHref="#b" />
       </g>
     </svg>
   )
--- src/components/MyCartButton/styles.js
+++ src/components/MyCartButton/styles.js
@@ -3,27 +3,24 @@ import variables from 'styles/variables'
 
 export const Container = styled.div`
   align-items: center;
+  background: ${variables.secondaryColor};
+  border-radius: 5px;
   color: ${variables.whiteColor};
   display: grid;
   grid-column-start: ${({ gridColumn }) => (gridColumn || 1)};
-  grid-template-columns: 85% 15%;
-  text-align: center;
-`
-export const Name = styled.p`
- background: ${variables.secondaryColor};
- border-bottom-left-radius: 3px;
- border-top-left-radius: 3px;
- display: grid;
- justify-content: center;
- margin-left: auto;
- padding: 10px 20px;
+  grid-template-columns: 25% 50% 25%;
+  height: 50px;
+  justify-items: center;
+  justify-self: end;
+  width: 60%;
 `
 
 export const Value = styled.p`
-background: ${variables.quinaryColor};
-border-bottom-right-radius: 3px;
-border-top-right-radius: 3px;
-display: grid;
-justify-content: center;
-padding: 10px 20px;
+  align-items: center;
+  background: ${variables.quinaryColor};
+  border-radius: 0px 5px 5px 0px;
+  display: grid;
+  height: 100%;
+  text-align: center;
+  width: 100%;
 `
